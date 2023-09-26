a49e841cf71b81a68646910b3f507fa70449f33f
Ahmed Salih Cezayir <asalihcezayir@gmail.com>
2023-09-26 13:14:06 +0300

    Resolved problem with TODO-9 and around aspect

--- src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/LoggingAspect.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/LoggingAspect.java
@@ -54,7 +54,7 @@ public class LoggingAspect {
     // - Mark this method as an around advice.
 	// - Write a pointcut expression to match on all update* methods
 	//	 on all Repository classes.
-	@Around("execution(* *Repository.update*(..))")
+	@Around("execution(* *..*Repository.update*(..))")
 	public Object monitor(ProceedingJoinPoint repositoryMethod) throws Throwable {
 		String name = createJoinPointTraceName(repositoryMethod);
 		Monitor monitor = monitorFactory.start(name);
